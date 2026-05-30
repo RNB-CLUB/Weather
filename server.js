352e05bfee0e08fd82b193e6a7af337281fd2ac4
Lettyks <sofiakabanets@icloud.com>
2026-05-30 17:14:57 +0300

    sdawa

---
+++ server.js
@@ -0,0 +1,17 @@
+import { createServer } from http
+
+
+
+const server = createServer(async (req , res){
+    try{
+
+    }catch(error){
+        console.log("AAAaaaa", error.message)
+    }
+})
+
+
+function pathToFile(){
+    const 
+
+}
