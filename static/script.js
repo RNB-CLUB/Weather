e403865c78a8c05d4f2d7e3417a1ebdfe123d3c2
ParmValeriia <parmenovavaleria@gmail.com>
2026-05-30 17:43:09 +0300

    wgfuqgfhfj

--- static/script.js
+++ static/script.js
@@ -1,4 +1,14 @@
 let api_key = "a85e06d2a31c43d9bc4135230263005"
 let base_url = "https://api.weatherapi.com/v1/current.json?key=" + api_key
 
-fetch(base_url + "&q=london").then(res=>res.json()).then(console.log)
+
+let weather;
+fetch(base_url + "&q=london").then(res => res.json()).then(data => {
+    weather = data
+    updateWeather()
+}
+)
+
+function updateWeather() {
+    document.querySelector(".current-weather .icon").setAttribute("src", weather.current.condition.icon)
+}
