e27dc3122c7207f9f830ce22c355c5243214e73a
RNB-CLUB <daniil.ct318@gmail.com>
2026-06-06 16:03:08 +0300

    fs

--- static/script.js
+++ static/script.js
@@ -2,3 +2,4 @@ let api_key = "a85e06d2a31c43d9bc4135230263005"
 let base_url = "https://api.weatherapi.com/v1/current.json?key=" + api_key
 
 fetch(base_url + "&q=london").then(res=>res.json()).then(console.log)
+
